4d5ae3be5f458352d06886d8f809decc066e75ae
Martin VANAUD <martin.vanaud@epitech.eu>
2023-01-01 19:13:59 +0100

    feat(etudiants routes) added route to udpate and get a student profile

--- handlers/etudiants/etudiants.go
+++ handlers/etudiants/etudiants.go
@@ -4,10 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"juniorconseiltaker-siaje-api/server"
 	"net/http"
 	"net/url"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -53,13 +55,132 @@ type EtudiantPayload struct {
 
 func Get(context *gin.Context) {
 
+	requestId := context.Request.URL.Query().Get("id")
+
+	if requestId == "" {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"code":    http.StatusBadRequest,
+			"message": "Missing parameter id",
+		})
+	} else {
+		if _, err := strconv.Atoi(requestId); err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{
+				"code":    http.StatusBadRequest,
+				"message": "Invalid parameter id",
+			})
+		}
+	}
+
+	siajeStudentGetEndpoint := fmt.Sprintf("https://pro.siaje.com/jctaker/etudiants.php?mode=edit&etudiant=%s", requestId)
+	method := "GET"
+
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+		Jar: server.CookieJar,
+	}
+	req, err := http.NewRequest(method, siajeStudentGetEndpoint, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
+
+	etudiant := make(map[string]string)
+
+	const void string = ""
+
+	htmlParserRegex := regexp.MustCompile("<head>(?:.|\\n|\\r)+?<\\/head>")
+
+	firstBatch := htmlParserRegex.ReplaceAllString(string(body), void)
+
+	htmlParserRegex = regexp.MustCompile("<div(?:.|\\n|\\r)+?<div id=\"content\" mode=\"\">")
+
+	secondBatch := htmlParserRegex.ReplaceAllString(firstBatch, void)
+
+	htmlParserRegex = regexp.MustCompile("<script(?:.|\\n|\\r)+?<\\/script>")
+
+	thirdBatch := htmlParserRegex.ReplaceAllString(secondBatch, void)
+
+	htmlParserRegex = regexp.MustCompile("<!-- #suggestions -->(?:.|\\n|\\r)+?<\\/div><!-- fin new_top -->")
+
+	studentSectionContent := htmlParserRegex.ReplaceAllString(thirdBatch, void)
+
+	htmlTagRegex := regexp.MustCompile(`(?P<tag>id|name|value)="(?P<item>.*?)"`)
+
+	htmlTagMatch := htmlTagRegex.FindAllStringSubmatch(string(studentSectionContent), -1)
+
+	for index := 0; index < len(htmlTagMatch); index += 1 {
+
+		if index+2 >= len(htmlTagMatch) {
+			break
+		}
+
+		if htmlTagMatch[index][1] == "id" && htmlTagMatch[index+1][1] == "name" && htmlTagMatch[index+2][1] == "value" {
+			etudiant[htmlTagMatch[index+1][2]] = htmlTagMatch[index+2][2]
+		}
+	}
+
+	textareaTagRegex := regexp.MustCompile(`(?:<textarea)(.|\n|\r)+?(<\/textarea>)`)
+	textTagRegex := regexp.MustCompile(`id="([^"]+)"`)
+	textContentRegex := regexp.MustCompile(`(?:class="">)((?:.|\n|\r)+?)(?:<\/textarea>)`)
+
+	textarea := textareaTagRegex.FindAllStringSubmatch(string(studentSectionContent), -1)
+
+	for item := 0; item < len(textarea); item += 1 {
+
+		text := strings.Join(textarea[item], " ")
+		match_id := textTagRegex.FindAllStringSubmatch(text, 1)
+		match_text := textContentRegex.FindAllStringSubmatch(text, -1)
+
+		if match_id[0][1] == "adresse" {
+
+			endOfLine := match_text[0][1][len(match_text[0][1])-1:]
+			if endOfLine == "\n" {
+				match_text[0][1] = match_text[0][1][:len(match_text[0][1])-1]
+			}
+
+			etudiant[match_id[0][1]] = match_text[0][1]
+		}
+
+	}
+
+	birthDateRegex := regexp.MustCompile(`(?:selected="selected">)((.|\n|\r)+?)(?:<\/option>)`)
+
+	birthDateRegexMatch := birthDateRegex.FindAllStringSubmatch(string(studentSectionContent), -1)
+
+	const day int = 1
+	const month int = 2
+	const year int = 3
+
+	etudiant["date_naissance[jour]"] = birthDateRegexMatch[day][1]
+	etudiant["date_naissance[mois]"] = birthDateRegexMatch[month][1]
+	etudiant["date_naissance[annee]"] = birthDateRegexMatch[year][1]
+
+	context.JSON(http.StatusOK, gin.H{
+		"code":     http.StatusOK,
+		"message":  "Found student",
+		"etudiant": etudiant,
+	})
 }
 
 func Create(context *gin.Context) {
 
 	siajeStudentCreateEndpoint := "https://pro.siaje.com/jctaker/etudiants.php?mode=add"
-
-	const successfulRequestUrl string = "https://pro.siaje.com/jctaker/etudiants.php?etudiant=" // + regex((0-9)+)
 
 	method := "POST"
 
@@ -124,6 +245,10 @@ func Create(context *gin.Context) {
 			match, _ := regexp.MatchString("(https:\\/\\/pro.siaje.com\\/jctaker\\/etudiants.php\\?etudiant=)([0-9]+)", forwardUrl)
 
 			if match {
+
+				split := strings.SplitAfter(forwardUrl, "=")
+				fmt.Println("id:", split[1])
+
 				requestSuccessful = true
 				return http.ErrUseLastResponse
 			}
@@ -166,5 +291,9 @@ func Create(context *gin.Context) {
 }
 
 func Update(context *gin.Context) {
+
+}
+
+func Delete(context *gin.Context) {
 
 }
